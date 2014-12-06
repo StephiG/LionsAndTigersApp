23701c7e3beb23f78e51af84d72fe3b017f4ffa4
Stephi Goering <stephigoering@Stephis-Air-3.local>
2014-12-06 08:59:11 +0100

    Initial Commit

---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by Stephi Goering on 06/12/14.
+//  Copyright (c) 2014 Stephi Goering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
